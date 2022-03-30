86b7f54977f9b6692764f8abe276614a6a5a1ac1
pryme0 <obochi2@gmail.com>
2022-03-30 05:02:25 +0100

    removed unused code in server.js, add customer service folder

--- server.js
+++ server.js
@@ -21,8 +21,7 @@ require("./db/dbMongo/config/db_connection");
 const app = express();
 const port = process.env.PORT || 5000;
 const facebook = require("./routes/facebook");
-// const bodyParser = require("body-parser");
-// require('./db/dbPostgress/config/db_addAdmin.sql')();
+
 
 //----------------------------------------------------------------------------------
 const { authenticateLoginToken, } = require("./controllers/authentication/1.authenticateLoginToken");
@@ -103,10 +102,7 @@ var storage = multer.diskStorage({
 var storage2 = multer2.diskStorage({
   destination: './multerFilesToDBs',
   filename: function (req, file, cb) {
-    // console.log("In chunks multer where files are:");
-    // console.log(file);
-    // console.log("req is");
-    // console.log(req);
+
     const str1 = randomString().replace("+", "").replace("-", "").replace("/", "").replace("*", "").replace("/", "").replace("?", "")
     cb(null, str1 + file.originalname)
   }
@@ -123,9 +119,7 @@ app.get("/get_store_products", async (req, res) => {
   console.log("Calling servers get_store_products to Mongo");
   //Check if we connected to the database or not
   let db = mongoose.connection;
-  // let vari =  await db.collection("Store_Products").countDocuments();
-  // console.log(vari);
-  // console.log(vari);
+
   db.on("error", console.error.bind(console, "Connection error:"));
   await db.once("open", function () {
     console.log("We are supposedly connected to the Mongo database");
@@ -177,11 +171,6 @@ app.get("/test", (req, res) => {
   res.send(JSON.stringify(req.session));
 });
 
-// app.get("/redirect", (req, res) => {
-//   console.log("To redirect page");
-//   res.sendFile(path.join(__dirname + "/client", "public", "index.html"));
-// });
-
 app.get("/renderEJS", (req, res) => {
   console.log("To render ");
   res.render("index");
@@ -204,23 +193,9 @@ app.get("/terms-of-service", (req, res) => {
 
 
 app.post("/api/login", authenticateLoginToken);
-// app.post("/api/forgotpass", forgotPassword);
-// app.post("/api/resetpass", resetPassword);
-// app.post("/api/signupuser", signupCustomer);
-// app.post("/api/signup/:newcustomerId", authenticationSignup);
+
 app.post("/api/send-mealData", sendMealtable);
 
-// app.post("/api/addMealSuggestion/", addMealSuggestion );
-
-// app.post("/api/addMealSuggestion/", upload.single('mealImage'));
-// app.post("/api/addMealSuggestion/", upload.single('mealImage'), addMealSuggestion);
-
-
-// handle Instruction content upload
-//***********************************************************************************
-
-// console.log(instructionContentStorageConfigs);
-// var multerInstanciatedWithStorageConfigs = instructionContentMulter({ storage: instructionContentStorageConfigs });
 var multerOptionsToAcceptIntstructionContent = [
   { name: 'mealImage' },
   { name: 'instructionChunkContent1' },
@@ -230,30 +205,8 @@ var multerOptionsToAcceptIntstructionContent = [
   { name: 'instructionChunkContent5' },
   { name: 'instructionChunkContent6' },
 ]
-// const imageAndVideoContentFromForm = upload.fields([{ name: 'mealImage' },
-// { name: 'instructionChunkContent1' },
-// { name: 'instructionChunkContent2' },
-// { name: 'instructionChunkContent3' },
-// { name: 'instructionChunkContent4' },
-// { name: 'instructionChunkContent5' },
-// { name: 'instructionChunkContent6' },
-// ]);
-
-// app.post("/api/addMealSuggestion/", upload.single('mealImage'), writeFile, addMealSuggestion);
 
 app.post("/api/addMealSuggestion/", upload2.fields(multerOptionsToAcceptIntstructionContent), writeFile, transferToS3, addMealSuggestion);
-
-// app.post("/api/addMealSuggestion/", upload2.fields(multerOptionsToAcceptIntstructionContent), writeFile,
-//   function (req, res, next) {
-//     console.log("Comes in multer handler function ");
-//     console.log("req body");
-//     console.log(req.body);
-//     console.log("req file name");
-//     console.log(req.files.instructionChunkContent1[0].originalname);
-//     console.log(req.files.instructionChunkContent1[0].filename);
-//     console.log(req.files);
-//     next();
-//   }, transferToS3, addMealSuggestion);
 
 
 // test multer logging
@@ -263,313 +216,6 @@ app.use((error) => {
 });
 
 
-// Test for multer error
-// if(err instanceof instructionContentMulter.MulterError){
-//   console.log("There is an error");
-//   console.log(error.field);
-// }
-// app.post('/api/appendItem',appendItem)
-
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-
-
-// const { S3Client } = require("@aws-sdk/client-s3");
-// const client = new S3Client({
-//   region: process.env.S3_REGION,
-//   accessKeyId: process.env.CHOPCHOWAPP_USER_AWS_KEY,
-//   SecretAccessKey: process.env.CHOPCHOWAPP_USER_AWS_SECRET
-// });
-
-// var multerS3 = require('multer-s3')
-
-// var upload = multer({ storage: storage });
-// var multer2 = require('multer');
-// var s3upload = multer2({
-//   storage: new multerS3({
-//     s3: client,
-//     bucket: process.env.S3_BUCKET,
-//     metadata: function (req, file, cb) {
-//       cb(null, { fieldName: file.fieldname });
-//     },
-//     key: function (req, file, cb) {
-//       cb(null, Date.now().toString())
-//     }
-//   })
-// })
-  
-// app.post("/api/addMealInstructionContent/", () => {
-//   console.log("Comes in new handler with function ");
-//   addMealInstructionContent
-// });
-// app.post("/api/addMealInstructionContent/", (req, res, next) => {
-// }, addMealInstructionContent);
-
-
-// // var productImg_upload = productImg_multer( { storage: productImg_storage } );
-
-//***********************************************************************************
-// var productImg_multer  = require('multer');
-// var productImg_storage = productImg_multer.diskStorage(
-//   {
-//       destination: 'client/build/uploads/products/',
-//       filename: function ( req, file, cb ) {
-//           const str1=randomString().replace("+","").replace("-","").replace("/","").replace("*","").replace("/","").replace("?","")
-//           cb( null, str1+"_"+file.originalname);
-//       }
-//   }
-// );
-// var productImg_upload = productImg_multer( { storage: productImg_storage } );
-// const {  getProductImgPath,} = require("./db/dbMongo/queries/mealsAPI/getProductImgPath");
-// app.post("/api/getProductImgURL/", function (req, res) {
-//   productImg_upload.array('productImgs')
-//   }, getProductImgPath);
-
-
-//***********************************************************************************
-// var multer  = require('multer');
-// var storage = multer.diskStorage(
-//   {
-//       destination: 'client/build/uploads/',
-//       filename: function ( req, file, cb ) {
-//           const str1=randomString().replace("+","").replace("-","").replace("/","").replace("*","").replace("/","").replace("?","")
-//           cb( null, str1+"_"+file.originalname);
-//       }
-//   }
-// );
-// var upload = multer( { storage: storage } );
-// app.post("/api/addMealInstructionContent/", addMealInstructionContent);
-
 app.post("/api/updateSuggestedMealItem/", upload.array('imgSrc'), updateSuggestedMealItem);
-
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-// app.get('*', (req, res) => {
-//     console.log("Gets in client builds index");
-//     res.sendFile(path.join(__dirname+'/client/build/'));
-//   });
-
-// app.get('/find', function (req, res) {
-//     app.get('/api/get-all-lists', getAllLists)
-//     console.log("Gets in servers find route.....");
-//     if (!req.session.cart) {
-//         console.log("Creates a cart session")
-//         req.session.cart = {
-//             items: [],
-//             totals: 0.00,
-//             formattedTotals: ''
-//         };
-//     }
-//     else {
-//         console.log("Cart exists");
-//         console.log(req.session);
-//     }
-// Products.find({price: {'$gt': 0}}).sort({price: -1}).limit(6).then(products => {
-//     console.log(products);
-//     let format = new Intl.NumberFormat(req.app.locals.locale.lang, {style: 'currency', currency: req.app.locals.locale.currency });
-//     products.forEach( (product) => {
-//        product.formattedPrice = format.format(product.price);
-//     });
-
-//     res.render('index', {
-//         pageTitle: 'Node.js Shopping Cart',
-//         products: products,
-//         nonce: Security.md5(req.sessionID + req.headers['user-agent'])
-//     });
-
-// }).catch(err => {
-//     res.status(400).send('Bad request');
-// });
-// console.log("ends db search")
-// }
-// );
-// after identifying unique  session tokens from MD5 string
-// Then we are able to compare tokens in each singular form request:
-// app.post('/test', (req, res) => {
-//     let token = req.body.nonce;
-//     if(Security.isValidNonce(token, req)) {
-//       console.log("Post route test of valid Nonce says OK");
-
-//     } else {
-//       // Reject the request
-//       console.log("Post route test of valid Nonce is REJECTED!");
-
-//     }
-// });
-
-// app.get('/cart', (req, res) => {
-//     console.log("Gets in cart route");
-//     let sess = req.session;
-//     console.log(sess);
-//     let cart = (typeof sess.cart !== 'undefined') ? sess.cart : false;
-//     res.render('cart', {
-//         pageTitle: 'Cart',
-//         cart: cart,
-//         nonce: Security.md5(req.sessionID + req.headers['user-agent'])
-//     });
-// });
-
-// app.get('/cart/remove/:id/:nonce', (req, res) => {
-//    let id = req.params.id;
-//    if(/^\d+$/.test(id) && Security.isValidNonce(req.params.nonce, req)) {
-//        Cart.removeFromCart(parseInt(id, 10), req.session.cart);
-//        res.redirect('/cart');
-//    } else {
-//        res.redirect('/');
-//    }
-// });
-
-// app.get('/cart/empty/:nonce', (req, res) => {
-//     if(Security.isValidNonce(req.params.nonce, req)) {
-//         Cart.emptyCart(req);
-//         res.redirect('/cart');
-//     } else {
-//         res.redirect('/');
-//     }
-// });
-
-// app.post('/cart', (req, res) => {
-//     let qty = parseInt(req.body.qty, 10);
-//     let product = parseInt(req.body.product_id, 10);
-//     if(qty > 0 && Security.isValidNonce(req.body.nonce, req)) {
-//       Products.findOne({product_id: product}).then(prod => {
-//           Cart.addToCart(prod, qty);
-//           Cart.saveCart(req);
-//           res.redirect('/cart');
-//       }).catch(err => {
-//          res.redirect('/');
-//       });
-//   } else {
-//       res.redirect('/');
-//   }
-//   });
-
-//   app.post('/cart/update', (req, res) => {
-//     let ids = req.body["product_id[]"];
-//     let qtys = req.body["qty[]"];
-//     if(Security.isValidNonce(req.body.nonce, req)) {
-//         let cart = (req.session.cart) ? req.session.cart : null;
-//         let i = (!Array.isArray(ids)) ? [ids] : ids;
-//         let q = (!Array.isArray(qtys)) ? [qtys] : qtys;
-//         Cart.updateCart(i, q, cart);
-//         Cart.saveCart(req);
-//         res.redirect('/cart');
-//     } else {
-//         res.redirect('/');
-//     }
-//     });
-
-//     app.get('/checkout', (req, res) => {
-//         let sess = req.session;
-//         let cart = (typeof sess.cart !== 'undefined') ? sess.cart : false;
-//         res.render('checkout', {
-//             pageTitle: 'Checkout',
-//             cart: cart,
-//             checkoutDone: false,
-//             nonce: Security.md5(req.sessionID + req.headers['user-agent'])
-//         });
-//     });
-
-//     app.post('/checkout', (req, res) => {
-//         let sess = req.session;
-//         let cart = (typeof sess.cart !== 'undefined') ? sess.cart : false;
-//         if(Security.isValidNonce(req.body.nonce, req)) {
-//             res.render('checkout', {
-//                 pageTitle: 'Checkout',
-//                 cart: cart,
-//                 checkoutDone: true
-//             });
-//         } else {
-//             res.redirect('/');
-//         }
-//     });
-
-// const { parse } = require('url');
-// require('dotenv').config();
-
-//  let { DATABASE_URL: dbUrl } = process.env;
-
-// const params = parse(dbUrl);
-// const {
-//   hostname: host, port, pathname, auth,
-// } = params;
-
-// const [user, password] = auth.split(':');
-
-// console.log("port:",port);
-// console.log("host:",host);
-// console.log("pathname:",pathname);
-// console.log("auth:",auth);
-// console.log("user:",user);
-// console.log("password:",password);
-//  const { readFileSync } = require('fs');
-// const { join } = require('path');
-// const dbconnection = require('./db/dbPostgress/config/db_connection');
-// var sql = "CREATE TABLE ttt (id SERIAL PRIMARY KEY,  firstname TEXT,  lastname TEXT,  email TEXT,  password TEXT,  phoneNumber NUMERIC,  street TEXT,  city TEXT,  zipCode INTEGER,  ipsid INTEGER,  username TEXT,  emailnotifcation BOOLEAN,  passwordtoken TEXT,  list_id NUMERIC)";
-// dbconnection.query(sql, function(err, result){
-//   if(err)
-//     throw err;
-//   console.log("Database Created");
-// });
-// const dbBuild = () => {
-//   new Promise((resolve, reject) => {
-//     console.log('111');
-//       readFileSync(join(__dirname, 'db_build.sql'), (errInFindingFile, sql) => {
-//         console.log('222');
-//           if (errInFindingFile) reject(errInFindFile);
-//           dbconnection(sql)
-//               .then(() => {
-//                   console.log('Database was built successfully');
-//                   resolve(true)
-//               }).catch((errInQuery) => {
-//                 console.log('333');
-//                   reject(errInQuery)
-//               });
-//       });
-//   })
-// }
-// dbBuild();
-
-
-// ----------------------------------------------------------------------------------------
-// This was inside of handling mealImage
- //  (req, res, next) => {
-//   // function (err) {
-//   //   if (err instanceof multer.MulterError) {
-//   //     // A Multer error occurred when uploading.
-//   //   } else if (err) {
-//   //     // An unknown error occurred when uploading.
-//   //   }
-//     console.log("should have stored in multers dest folder already, so now req.file is:");
-//     console.log(req.file);
-//     // console.log("vs");
-//     // console.log(req.files);
-//     // console.log("vs");
-//     console.log(req.body);
-//     var mealImageData = {
-//       name: req.file.filename,
-//       data: fs.readFileSync(path.join(__dirname + '/imageToDB/' + req.file.filename)),
-//       contentType: req.file.mimetype
-//     }
-//     console.log(mealImageData);
-//     meal_images.create(mealImageData, (err, item) => {
-//       if (err) {
-//         console.log("Fails to add meal image");
-//         console.log(err);
-//         res.status(404).send(JSON.stringify({ msg: 'Failed to add image to gridfs', done: false }))
-
-//       }
-//       else {
-//         // item.save();
-//         console.log("Succesfully added meal image to db");
-//         // res.status(200).send(JSON.stringify({ msg: 'Succesfully added', done: true }))
-//       }
-//     });
-//     // res.status(204).send(JSON.stringify({ msg: 'Succesfully added', done: true }))
-//     // console.log("sent image data");
-//     // res.json({ fields, files });
-//     // });
-//     next();
-//   }
-// ---------------------------------------------------------------------------------
